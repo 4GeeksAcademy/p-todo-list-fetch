c2ab0880f0837700847eb25d3bfe4cd52908421a
Dimatto404 <dimatto404@gmail.com>
2024-10-11 22:48:35 +0000

    p-todo-list

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -1,24 +1,40 @@
-import React from "react";
+import React, { useState } from "react";
 
-//include images into your bundle
-import rigoImage from "../../img/rigo-baby.jpg";
-
-//create your first component
 const Home = () => {
+	const [todo, setTodo] = useState([]);
+	const [aux, setAux] = useState("");
+
+	const eliminarTarea = (index) => {
+		const nuevasTareas = todo.filter((elemento, i) => i !== index);
+		setTodo(nuevasTareas);
+	};
+
 	return (
-		<div className="text-center">
-			<h1 className="text-center mt-5">Hello Rigo!</h1>
-			<p>
-				<img src={rigoImage} />
-			</p>
-			<a href="#" className="btn btn-success">
-				If you see this green button... bootstrap is working...
-			</a>
-			<p>
-				Made by{" "}
-				<a href="http://www.4geeksacademy.com">4Geeks Academy</a>, with
-				love!
-			</p>
+		<div className="text-center bg-light" style={{ height: "550px" }}>
+			<h1 className="text-center mt-5 text-muted">t o d o s</h1>
+			<br />
+			<div className="container list-group">
+				<input
+					type="text"
+					className="list-group-item" value={aux} placeholder="What needs to be done?"
+					onChange={(e) => setAux(e.target.value)}
+					onKeyDown={(e) => {
+						if (e.key === "Enter" && aux.trim() !== "") {
+							setTodo([...todo, aux]);
+							setAux("");
+						}
+					}}
+				/>
+				<ul className="list-group-item list-unstyled">
+					{todo.length === 0 ? (<li className="todo-item">There are no current tasks</li>) :
+						(todo.map((task, index) => (<li className="todo-item" key={index}>{task}
+							<button className="btn d-btn btn-sm float-end" onClick={() => eliminarTarea(index)}>X</button>
+						</li>)))}
+					<li className="todo-item">
+						{todo.length} tasks left
+					</li>
+				</ul>
+			</div>
 		</div>
 	);
 };
