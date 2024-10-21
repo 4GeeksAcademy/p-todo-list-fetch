8a51f7579d5a5d4eb8ce38d1cddd0f290a594e1e
Dimatto404 <dimatto404@gmail.com>
2024-10-21 23:43:35 +0000

    p-todo-list-fetch

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -1,37 +1,92 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 
 const Home = () => {
-	const [todo, setTodo] = useState([]);
+
+	const [todos, setTodos] = useState([]);
 	const [aux, setAux] = useState("");
 
-	const eliminarTarea = (index) => {
-		const nuevasTareas = todo.filter((elemento, i) => i !== index);
-		setTodo(nuevasTareas);
+	const fetchTodos = () => {
+		fetch(`https://playground.4geeks.com/todo/users/dimatto404`)
+			.then((resp) => resp.json())
+			.then((data) => {
+				console.log(data);
+				setTodos(data.todos);
+			})
+			.catch((error) => console.error(error));
 	};
+
+	useEffect(() => {
+		fetchTodos();
+	}, []);
+
+	function agregarTarea(nuevaTarea) {
+		const Tarea = {
+			label: nuevaTarea,
+			is_done: false
+		};
+		fetch(`https://playground.4geeks.com/todo/todos/dimatto404`, {
+			method: "POST",
+			body: JSON.stringify(Tarea),
+			headers: {
+				"Content-Type": "application/json"
+			}
+		})
+			.then((response) => response.json())
+			.then(() => {
+				fetchTodos();
+			})
+			.catch((error) => console.error(error));
+	}
+
+	function eliminarTarea(id) {
+		fetch(`https://playground.4geeks.com/todo/todos/${id}`, {
+			method: "DELETE",
+			headers: {
+				"Content-Type": "application/json"
+			}
+		})
+			.then((data) => {
+				console.log(data);
+				fetchTodos();
+			})
+			.catch((error) => console.error(error));
+	}
+
+	function eliminarTodasLasTareas() {
+		todos.forEach((todo) => eliminarTarea(todo.id));
+	}
 
 	return (
 		<div className="text-center bg-light" style={{ height: "550px" }}>
 			<h1 className="text-center mt-5 text-muted">t o d o s</h1>
+			<button className="btn btn-secondary m-3 " onClick={eliminarTodasLasTareas}>
+				All is done!
+			</button>
 			<br />
 			<div className="container list-group">
 				<input
 					type="text"
-					className="list-group-item" value={aux} placeholder="What needs to be done?"
+					className="list-group-item"
+					value={aux}
+					placeholder="What needs to be done?"
 					onChange={(e) => setAux(e.target.value)}
 					onKeyDown={(e) => {
 						if (e.key === "Enter" && aux.trim() !== "") {
-							setTodo([...todo, aux]);
+							agregarTarea(aux);
 							setAux("");
 						}
 					}}
 				/>
 				<ul className="list-group-item list-unstyled">
-					{todo.length === 0 ? (<li className="todo-item">There are no current tasks</li>) :
-						(todo.map((task, index) => (<li className="todo-item" key={index}>{task}
-							<button className="btn d-btn btn-sm float-end" onClick={() => eliminarTarea(index)}>X</button>
-						</li>)))}
+					{todos.length === 0 ? (<li className="todo-item">There are no current tasks</li>) :
+						(todos.map((todo, index) => (
+							<li className="todo-item" key={index}>
+								{todo.label}
+								<button className="btn d-btn btn-sm float-end" onClick={() => eliminarTarea(todo.id)}>X</button>
+							</li>
+						)))}
 					<li className="todo-item">
-						{todo.length} tasks left
+						{todos.length} tasks left
 					</li>
 				</ul>
 			</div>
